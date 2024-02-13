8e00d679a27fbbe62215fce45d1d5ca341073525
Codie Tamida <codietamida@gmail.com>
2024-02-12 23:14:50 -0800

    Added function to check if integer

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@
 def isIdentifier(identifier):
     
     identifier = identifier.lower()
-    
+
     if not identifier:
         return False
 
@@ -14,4 +14,13 @@ def isIdentifier(identifier):
         if not (s.isalnum() or s == '_'):
             return False
         
+    return True
+
+def isInteger(integer):
+    if not integer:
+        return False
+    ints = ['1','2','3','4','5','6','7','8','9','0']
+    for s in integer:
+        if s not in ints:
+            return False
     return True
