70a866c66b7f4e98eb06173aae7a131302f350a5
Codie Tamida <codietamida@gmail.com>
2024-03-27 21:05:35 -0700

    Rule 20 implemented

---
+++ tests/test_SA_r20_print.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+from tests.helpers import write_to_file, get_result_from_parser
+
+
+class PrintTestCase(unittest.TestCase):
+    SAMPLE_FILE_PATH = "tests/sample1.txt"
+
+    def setUp(self):
+        if os.path.exists(self.SAMPLE_FILE_PATH):
+            os.remove(self.SAMPLE_FILE_PATH)
+
+    def tearDown(self):
+        if os.path.exists(self.SAMPLE_FILE_PATH):
+            os.remove(self.SAMPLE_FILE_PATH)
+
+    def no_print_keyword(self):
+        input_string = " $$ ( a + b ); $"
+        expected_output= False
+
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        self.assertEqual(actual_output, expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
