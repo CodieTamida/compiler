1774d8c75b9295877cf46a2ec69cd32fb9a73271
Codie Tamida <codietamida@gmail.com>
2024-04-02 00:44:28 -0700

    Simple unittests added for r18 <If>

---
+++ tests/test_SA_r18_if.py
@@ -0,0 +1,59 @@
+import unittest
+import os
+from tests.helpers import write_to_file, get_result_from_parser
+
+
+class ReturnTestCase(unittest.TestCase):
+    SAMPLE_FILE_PATH = "tests/sample1.txt"
+
+    def setUp(self):
+        if os.path.exists(self.SAMPLE_FILE_PATH):
+            os.remove(self.SAMPLE_FILE_PATH)
+
+    def tearDown(self):
+        if os.path.exists(self.SAMPLE_FILE_PATH):
+            os.remove(self.SAMPLE_FILE_PATH)
+
+    def test_if_no_else(self):
+        input_string = "$ $ $ if (a == b ) return c; endif $"
+        expected_output = True
+
+        # Act
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+    
+    def test_if_else(self):
+        input_string = "$ $ $ if (a == b ) return c; else return 85; endif $"
+        expected_output = True
+
+        # Act
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_if_no_if(self):
+        input_string = "$ $ $ (a == b ) return c; endif $"
+        expected_output = False
+
+        # Act
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+    
+    def test_if_no_endif(self):
+        input_string = "$ $ $ if (a == b ) return c; $"
+        expected_output = False
+
+        # Act
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
