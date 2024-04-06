09f86199de87e12a3224afbd27c3a20be29c1a9c
Nguyen Nguyen <nltn@msn.com>
2024-04-06 14:28:15 -0700

    Added 4 test cases that include many statements

--- tests/unit/test_SA_r14_statement_list.py
+++ tests/unit/test_SA_r14_statement_list.py
@@ -14,10 +14,88 @@ class AssignTestCase(unittest.TestCase):
         if os.path.exists(self.SAMPLE_FILE_PATH):
             os.remove(self.SAMPLE_FILE_PATH)
 
-    def test_missing_a_statement(self):
+    def test_0_statements(self):
         # Arrange
         input_string = "$ $ $ $"
         expected_output = False
+
+        # Act
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_1_statement(self):
+        # Arrange
+        input_string = """
+                    $                         
+                    $ 
+                    $
+                        num = 1;
+                    $
+                    """
+        expected_output = True
+
+        # Act
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_2_statements(self):
+        # Arrange
+        input_string = """
+                    $                         
+                    $ 
+                    $
+                        num = 1;
+                        num = 2;
+                    $
+                    """
+        expected_output = True
+
+        # Act
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_3_statements(self):
+        # Arrange
+        input_string = """
+                    $                         
+                    $ 
+                    $
+                        num = 1;
+                        num = 2;
+                        num = 3;
+                    $
+                    """
+        expected_output = True
+
+        # Act
+        write_to_file(self.SAMPLE_FILE_PATH, input_string)
+        actual_output = get_result_from_parser(self.SAMPLE_FILE_PATH)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_4_statements(self):
+        # Arrange
+        input_string = """
+                    $                         
+                    $ 
+                    $
+                        num = 1;
+                        num = 2;
+                        num = 3;
+                        num = 4;
+                    $
+                    """
+        expected_output = True
 
         # Act
         write_to_file(self.SAMPLE_FILE_PATH, input_string)
