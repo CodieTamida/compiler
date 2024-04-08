76532f35a6812f030c18823084fab4c639b1c3b1
Nguyen Nguyen <nltn@msn.com>
2024-04-07 18:36:38 -0700

    Fixed a bug after a minus symbol, __r27_factor() prints the open parenthesis twice

--- components/syntax_analyzer.py
+++ components/syntax_analyzer.py
@@ -682,7 +682,8 @@ class Parser:
         if self.__current_token.lexeme == "-":
             self.__match(self.__current_token.lexeme)  # Move to the next token
             text_to_print = "<Factor> -> -"            
-            self.__log_current_token()
+            if self.__current_token.lexeme != "(":
+                self.__log_current_token()
         else:
             text_to_print = "<Factor> ->"
         
@@ -727,7 +728,6 @@ class Parser:
             self.__r25a_expression()
             self.__log_current_token()
             self.__match(")")  # Match and Move to the next token
-            self.__log("<Primary Prime> -> ( <Expression> )")
         # Handle error: The current token does not match any expected types
         else:
             raise ValueError(
